junit-team/junit5

Add & implement details printing listener interface
The purpose of this interface is to be able to extend the 3
different details printing listeners which inherit TestExecutionListener:
* TreePrintingListener
* VerboseTreePrintingListener
* FlatPrinting Listener

Issue: #2854

junit-platform-console/src/main/java/org/junit/platform/console/tasks/FlatPrintingListener.java
@@ -18,14 +18,13 @@ import java.util.regex.Pattern;
 import org.junit.platform.commons.util.ExceptionUtils;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.reporting.ReportEntry;
-import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
 /**
  * @since 1.0
  */
-class FlatPrintingListener implements TestExecutionListener {
+class FlatPrintingListener implements DetailsPrintingListener {
 
 	private static final Pattern LINE_START_PATTERN = Pattern.compile("(?m)^");
 
@@ -118,4 +117,10 @@ class FlatPrintingListener implements TestExecutionListener {
 		return LINE_START_PATTERN.matcher(message).replaceAll(INDENTATION).trim();
 	}
 
+	@Override
+	public void listTests(TestPlan testPlan) {
+		for (TestIdentifier testIdentifier : testPlan.getAllIdentifiers().values()) {
+			println(Color.SUCCESSFUL, "%s (%s)", testIdentifier.getDisplayName(), testIdentifier.getUniqueId());
+		}
+	}
 }

junit-platform-console/src/main/java/org/junit/platform/console/tasks/VerboseTreePrintingListener.java
@@ -20,14 +20,13 @@ import java.util.Deque;
 import org.junit.platform.console.options.Theme;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.reporting.ReportEntry;
-import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
 /**
  * @since 1.0
  */
-class VerboseTreePrintingListener implements TestExecutionListener {
+class VerboseTreePrintingListener implements DetailsPrintingListener {
 
 	private final PrintWriter out;
 	private final boolean disableAnsiColors;
@@ -191,4 +190,12 @@ class VerboseTreePrintingListener implements TestExecutionListener {
 		printf(NONE, "%n");
 	}
 
+	@Override
+	public void listTests(TestPlan testPlan) {
+		for (TestIdentifier testIdentifier : testPlan.getAllIdentifiers().values()) {
+			printVerticals(theme.entry());
+			printf(Color.valueOf(testIdentifier), " %s%n", testIdentifier.getDisplayName());
+			printDetails(testIdentifier);
+		}
+	}
 }

omandal/junit5f

junit-platform-console/src/main/java/org/junit/platform/console/tasks/FlatPrintingListener.java
@@ -16,14 +16,13 @@ import java.util.regex.Pattern;
 import org.junit.platform.commons.util.ExceptionUtils;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.reporting.ReportEntry;
-import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
 /**
  * @since 1.0
  */
-class FlatPrintingListener implements TestExecutionListener {
+class FlatPrintingListener implements DetailsPrintingListener {
 
 	private static final Pattern LINE_START_PATTERN = Pattern.compile("(?m)^");
 
@@ -106,4 +105,10 @@ class FlatPrintingListener implements TestExecutionListener {
 		return LINE_START_PATTERN.matcher(message).replaceAll(INDENTATION).trim();
 	}
 
+	@Override
+	public void listTests(TestPlan testPlan) {
+		for (TestIdentifier testIdentifier : testPlan.getAllIdentifiers().values()) {
+			println(Style.SUCCESSFUL, "%s (%s)", testIdentifier.getDisplayName(), testIdentifier.getUniqueId());
+		}
+	}
 }

junit-platform-console/src/main/java/org/junit/platform/console/tasks/VerboseTreePrintingListener.java
@@ -20,14 +20,13 @@ import java.util.Deque;
 import org.junit.platform.console.options.Theme;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.reporting.ReportEntry;
-import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
 /**
  * @since 1.0
  */
-class VerboseTreePrintingListener implements TestExecutionListener {
+class VerboseTreePrintingListener implements DetailsPrintingListener {
 
 	private final PrintWriter out;
 	private final Theme theme;
@@ -186,4 +185,12 @@ class VerboseTreePrintingListener implements TestExecutionListener {
 		printf(NONE, "%n");
 	}
 
+	@Override
+	public void listTests(TestPlan testPlan) {
+		for (TestIdentifier testIdentifier : testPlan.getAllIdentifiers().values()) {
+			printVerticals(theme.entry());
+			printf(Style.valueOf(testIdentifier), " %s%n", testIdentifier.getDisplayName());
+			printDetails(testIdentifier);
+		}
+	}
 }